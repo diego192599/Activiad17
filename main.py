73c81f854ed0219c5fe1e79bef73f92d2ecc7e0d
diego192599 <di.cramire@udeo.edu.gt>
2025-09-03 11:01:50 -0600

    funciones de sumar y multiplicar

--- main.py
+++ main.py
@@ -7,10 +7,27 @@ ventana.geometry("400*300")
 etiqueta=tk.Label(ventana, text="Ingrese dos numeros: ")
 etiqueta.pack(pady=6)
 
-entrada=tk.Entry(ventana)
-entrada.pack(pady=6)
+entrada1=tk.Entry(ventana)
+entrada1.pack(pady=6)
+
+entrada2=tk.Entry(ventana)
+entrada2.pack(pady=6)
+
+resultado=tk.Label(ventana,text="El resultado es:")
+resultado.pack(pady=6)
 
 def sumar():
-    num1=entrada.get()
-    num2=entrada.get()
+    try:
+        num1=float(entrada1.get())
+        num2=float(entrada2.get())
+        res=num1+num2
+        resultado.config(text=f"El resultado {res}")
+    except ValueError:
+        resultado.config(text="Error: ingrese números válidos")
+
+def multiplicar():
+    try:
+        num1=float(entrada1.get())
+        num2=float(entrada2.get())
+
 
